a082246d86bdfffb3f53bb0b4985c37503817d59
Koa Sato <koa.sato@gmail.com>
2020-03-14 13:24:45 -0700

    Fix Amazon make_weighted_graph.py

--- datasets/Amazon/make_weighted_graph.py
+++ datasets/Amazon/make_weighted_graph.py
@@ -1,7 +1,7 @@
 from random import randint
 
 with open('amazon0601.txt', 'r') as f:
-  with open ('amazon0601.txt', 'w') as f2:
+  with open ('amazon0601.txt_weighted', 'w') as f2:
    for line in f:
      f2.write(line[:-1] + ' ' + str(randint(1,100)) + "\n")
 
